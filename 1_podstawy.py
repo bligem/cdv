23234c1d408409b7fbe522ac56efee5c6c981320
Mateusz Blige <mblige@edu.cdv.pl>
2020-03-05 17:19:53 +0100

    asd

--- 1_podstawy.py
+++ 1_podstawy.py
@@ -1,1 +1,53 @@
 print("janusz")
+print(2)
+'''
+komentarz blokowy
+'''
+
+#w jednej linii
+
+potega = 2**10
+print(potega)
+
+#pobieranie danych z klawiatury
+#name=input()
+
+#print("twoje imie podane z klawiatury: "+name)
+
+#surname = input()
+#print ("imie: " + name + ", nazwisko: " + surname)
+
+
+#print("podaj swoj wiek: ", end="")
+#age = input()
+age1=21
+
+print(type(age1))
+###########################################
+surname = "a"
+surname="Kowalski"
+
+firstChar = surname[0]
+
+print(firstChar)
+
+lastChar = surname[-1]
+
+print(lastChar)
+
+#konwersja
+
+x = "5"
+
+print(type(x))
+
+x=int(x)
+
+print(type(x)) #int
+
+x=x/2
+
+print(type(x)) #float
+print(x)
+
+
